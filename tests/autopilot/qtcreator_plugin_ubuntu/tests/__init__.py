f071b4fd4d03e0d9eb3a0aa5290fe0351733778e
Christopher Lee <chris.lee@canonical.com>
2014-02-18 14:59:16 -0800

    Boilerplate code for initial test.

---
+++ tests/autopilot/qtcreator_plugin_ubuntu/tests/__init__.py
@@ -0,0 +1,13 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+# Copyright 2014 Canonical
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+
+"""qtcreator-plugin-ubuntu autopilot tests and emulators."""
+
+from autopilot.testcase import AutopilotTestCase
+
+class QtCreatorSideMenuTests(AutopilotTestCase):
+    pass
