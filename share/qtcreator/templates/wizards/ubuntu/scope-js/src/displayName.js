17748bc79b632903cb7047975067a851dba54a28
Marcus Tomlinson <marcus.tomlinson@canonical.com>
2015-10-13 10:16:27 +0200

    Update template to use OpenWeatherMap

--- share/qtcreator/templates/wizards/ubuntu/scope-js/src/displayName.js
+++ share/qtcreator/templates/wizards/ubuntu/scope-js/src/displayName.js
@@ -1,110 +1,176 @@
 var scopes = require('unity-js-scopes')
 var http = require('http');
 
-var query_host = "www.colourlovers.com"
-var query_path = "/api/palettes?format=json&numResults=100&keywords="
+var query_host = "api.openweathermap.org"
+var current_weather_path = "/data/2.5/weather?units=metric&APPID=2b12bf09b4e0ab0c1aa5e32a9a3f0cdc&q="
+var forecast_weather_path = "/data/2.5/forecast/daily/?units=metric&cnt=7&APPID=2b12bf09b4e0ab0c1aa5e32a9a3f0cdc&q="
+
+var CURRENT_TEMPLATE =
+{
+    "schema-version": 1,
+    "template": {
+        "category-layout": "grid",
+        "card-size": "medium"
+    },
+    "components": {
+        "title": "title",
+        "art": {
+            "field": "art"
+        },
+        "subtitle": "subtitle"
+    }
+}
+
+var FORECAST_TEMPLATE =
+{
+    "schema-version": 1,
+    "template": {
+        "category-layout": "grid",
+        "card-layout": "horizontal",
+        "card-size": "small"
+    },
+    "components": {
+        "title": "title",
+        "art" : {
+            "field": "art"
+        },
+        "subtitle": "subtitle"
+    }
+}
 
 scopes.self.initialize(
-    {}
-    ,
-    {
-        run: function() {
-            console.log('Running...')
-        },
-        start: function(scope_id) {
-            console.log('Starting scope id: '
-                        + scope_id
-                        + ', '
-                        + scopes.self.scope_config)
-        },
-        search: function(canned_query, metadata) {
-            return scopes.lib.new_search_query(
-                canned_query,
-                metadata,
-                // run
-                function(search_reply) {
-                    var qs = canned_query.query_string();
-                    var cat_title = "";
-                    if (!qs) {
-                        cat_title = "Best palettes"
-                    }
-                    var category = search_reply.register_category("simple", cat_title, "");
-
-                    var options = {
-                        host: query_host,
-                        path: query_path + qs
-                    };
-
-                    callback = function(response) {
-                        var res = '';
-
-                        // Another chunk of data has been recieved, so append it to res
-                        response.on('data', function(chunk) {
-                            res += chunk;
-                        });
-
-                        // The whole response has been recieved
-                        response.on('end', function() {
-                            r = JSON.parse(res);
-                            for (i = 0; i < r.length; i++) {
-                                var categorised_result = scopes.lib.new_categorised_result(category);
-                                categorised_result.set_uri(r[i].url);
-                                categorised_result.set_title(r[i].title);
-                                categorised_result.set_art(r[i].imageUrl);
-                                search_reply.push(categorised_result);
-
-                                // Limit result count to 15
-                                if (i == 14) {
-                                    break;
-                                }
-                            }
-                            search_reply.finished();
-                        });
-                    }
-
-                    http.request(options, callback).end();
+            {}
+            ,
+            {
+                run: function() {
+                    console.log('Running...')
                 },
-                // cancelled
-                function() {
-                });
-        },
-        preview: function(result, action_metadata) {
-            return scopes.lib.new_preview_query(
-                result,
-                action_metadata,
-                // run
-                function(preview_reply) {
-                    var layout1col = scopes.lib.new_column_layout(1);
-                    var layout2col = scopes.lib.new_column_layout(2);
-                    var layout3col = scopes.lib.new_column_layout(3);
-                    layout1col.add_column(["image", "header", "summary"]);
-
-                    layout2col.add_column(["image"]);
-                    layout2col.add_column(["header", "summary"]);
-
-                    layout3col.add_column(["image"]);
-                    layout3col.add_column(["header", "summary"]);
-                    layout3col.add_column([]);
-
-                    preview_reply.register_layout([layout1col, layout2col, layout3col]);
-
-                    var header = scopes.lib.new_preview_widget("header", "header");
-                    header.add_attribute_mapping("title", "title");
-                    header.add_attribute_mapping("subtitle", "subtitle");
-
-                    var image = scopes.lib.new_preview_widget("image", "image");
-                    image.add_attribute_mapping("source", "art");
-
-                    var description = scopes.lib.new_preview_widget("summary", "text");
-                    description.add_attribute_mapping("text", "description");
-
-                    preview_reply.push([image, header, description ]);
-                    preview_reply.finished();
+                start: function(scope_id) {
+                    console.log('Starting scope id: '
+                                + scope_id
+                                + ', '
+                                + scopes.self.scope_config)
                 },
-                // cancelled
-                function() {
-                });
-        }
-    }
-);
+                search: function(canned_query, metadata) {
+                    return scopes.lib.new_SearchQuery(
+                                canned_query,
+                                metadata,
+                                // run
+                                function(search_reply) {
+                                    var qs = canned_query.query_string();
+                                    if (!qs) {
+                                        qs = "London,uk"
+                                    }
 
+                                    var current_weather_cb = function(response) {
+                                        var res = '';
+
+                                        // Another chunk of data has been recieved, so append it to res
+                                        response.on('data', function(chunk) {
+                                            res += chunk;
+                                        });
+
+                                        // The whole response has been recieved
+                                        response.on('end', function() {
+                                            r = JSON.parse(res);
+
+                                            var category_renderer = new scopes.lib.category_renderer(JSON.stringify(CURRENT_TEMPLATE));
+                                            var category = search_reply.register_category("current", r.name + ", " + r.sys.country, "", category_renderer);
+
+                                            var categorised_result = new scopes.lib.categorised_result(category);
+                                            categorised_result.set_uri(r.id.toString());
+                                            categorised_result.set_title(r.main.temp.toString() + "°C");
+                                            categorised_result.set_art("http://openweathermap.org/img/w/" + r.weather[0].icon + ".png");
+                                            categorised_result.set("subtitle", r.weather[0].description);
+                                            categorised_result.set("description", "A description of the result");
+
+                                            search_reply.push(categorised_result);
+
+                                            // Now call back into the API for a 7 day forecast
+                                            http.request({host: query_host, path: forecast_weather_path + qs}, forecase_weather_cb).end();
+                                        });
+                                    }
+
+                                    var forecase_weather_cb = function(response) {
+                                        var res = '';
+
+                                        // Another chunk of data has been recieved, so append it to res
+                                        response.on('data', function(chunk) {
+                                            res += chunk;
+                                        });
+
+                                        // The whole response has been recieved
+                                        response.on('end', function() {
+                                            try {
+                                                r = JSON.parse(res);
+
+                                                var category_renderer = new scopes.lib.category_renderer(JSON.stringify(FORECAST_TEMPLATE));
+                                                var category = search_reply.register_category("forecast", "7 day forecast", "", category_renderer);
+
+                                                for (i = 0; i < r.list.length; i++) {
+                                                    var categorised_result = new scopes.lib.categorised_result(category);
+                                                    categorised_result.set_uri(r.list[i].weather[0].id.toString());
+                                                    categorised_result.set_title(r.list[i].temp.min.toString() + "°C to "
+                                                                                 + r.list[i].temp.max.toString() + "°C");
+                                                    categorised_result.set_art("http://openweathermap.org/img/w/" + r.list[i].weather[0].icon + ".png");
+                                                    categorised_result.set("subtitle", r.list[i].weather[0].description);
+                                                    categorised_result.set("description", "A description of the result");
+
+                                                    search_reply.push(categorised_result);
+                                                }
+
+                                                // We are done, call finished() on our search_reply
+                                                search_reply.finished();
+                                            }
+                                            catch(e) {
+                                                // Forecast not available
+                                                console.log("Forecast for '" + qs + "' is unavailable: " + e)
+                                            }
+                                        });
+                                    }
+
+                                    http.request({host: query_host, path: current_weather_path + qs}, current_weather_cb).end();
+                                },
+                                // cancelled
+                                function() {
+                                });
+                },
+                preview: function(result, action_metadata) {
+                    return scopes.lib.new_PreviewQuery(
+                                result,
+                                action_metadata,
+                                // run
+                                function(preview_reply) {
+                                    var layout1col = new scopes.lib.column_layout(1);
+                                    var layout2col = new scopes.lib.column_layout(2);
+                                    var layout3col = new scopes.lib.column_layout(3);
+                                    layout1col.add_column(["image", "header", "summary"]);
+
+                                    layout2col.add_column(["image"]);
+                                    layout2col.add_column(["header", "summary"]);
+
+                                    layout3col.add_column(["image"]);
+                                    layout3col.add_column(["header", "summary"]);
+                                    layout3col.add_column([]);
+
+                                    preview_reply.register_layout([layout1col, layout2col, layout3col]);
+
+                                    var header = scopes.lib.new_PreviewWidget("header", "header");
+                                    header.add_attribute_mapping("title", "title");
+                                    header.add_attribute_mapping("subtitle", "subtitle");
+
+                                    var image = scopes.lib.new_PreviewWidget("image", "image");
+                                    image.add_attribute_mapping("source", "art");
+
+                                    var description = scopes.lib.new_PreviewWidget("summary", "text");
+                                    description.add_attribute_mapping("text", "description");
+
+                                    preview_reply.push([image, header, description ]);
+                                    preview_reply.finished();
+                                },
+                                // cancelled
+                                function() {
+                                });
+                }
+            }
+            );
